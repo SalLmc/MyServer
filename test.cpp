6196a4c96f1eef3e2fb8e3c388856ecf018d8dd2
Sa1Lmc <sallmc@outlook.com>
2023-03-23 10:24:06 +0800

    3.23

--- test.cpp
+++ test.cpp
@@ -9,65 +9,19 @@
 #include <string.h>
 #include <sys/file.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <sys/time.h>
 #include <unordered_map>
 
-unsigned long long getTickMs()
+#include "src/core/core.h"
+
+int main()
 {
-    struct timeval now = {0};
-    gettimeofday(&now, NULL);
-    unsigned long long u = now.tv_sec;
-    u *= 1000;
-    u += now.tv_usec / 1000;
-    return u;
-}
+    ServerAttribute a(8081, "/home/sallmc/dist", "index.html", "/api/", "http://175.178.175.106:8080/", 0,{"index.html"});
 
-class Connection
-{
-  public:
-    int fd_;
-    sockaddr_in addr_;
-};
-
-int main(int argc, char *argv[])
-{
-    if (argc < 3)
+    for(auto &x:a.try_files)
     {
-        printf("usage: %s ip_address port_number\n", basename(argv[0]));
-        return 1;
-    }
-
-    int port = atoi(argv[2]);
-
-    Connection c;
-
-    inet_pton(AF_INET, argv[1], &c.addr_.sin_addr);
-    c.addr_.sin_family = AF_INET;
-    c.addr_.sin_port = htons(port);
-    c.fd_ = socket(AF_INET, SOCK_STREAM, 0);
-    assert(c.fd_ >= 0);
-
-    assert(connect(c.fd_, (sockaddr *)&c.addr_, sizeof(c.addr_)) != -1);
-
-    auto time0 = getTickMs();
-
-    char buffer[] = "HELLO";
-
-    send(c.fd_, buffer, sizeof(buffer), 0);
-
-    char recvBuffer[1024];
-    memset(recvBuffer, 0, sizeof(recvBuffer));
-    recv(c.fd_, recvBuffer, 1000, 0);
-
-    auto delta = getTickMs() - time0;
-    if (delta >= 5000)
-    {
-        printf("host unreachable\n");
-    }
-    else
-    {
-        printf("connect OK\n");
+        printf("%s\n",x.c_str());
     }
 }
