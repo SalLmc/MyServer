dfad213d78b93d2aa97ec1b51cca02b290274004
Sa1Lmc <sallmc@outlook.com>
2023-10-08 19:10:26 +0800

    10.8

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,25 @@
 #include "../src/headers.h"
+#include "../src/log/logger.h"
+#include "../src/utils/utils_declaration.h"
 
 using namespace std;
 
 int main()
 {
-    int cores = sysconf(_SC_NPROCESSORS_CONF);
-    printf("%d\n", cores);
+    Logger logger("log/", "test", 1);
+    string ip = "127.0.0.1";
+    int port = 8080;
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr_;
+    addr_.sin_family = AF_INET;
+    inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr);
+    addr_.sin_port = htons(port);
+    if (connect(fd, (struct sockaddr *)&addr_, sizeof(addr_)) < 0)
+    {
+        __LOG_INFO_INNER(logger) << "errno:" << strerror(errno);
+    }
+    else
+    {
+        printf("OK\n");
+    }
 }
